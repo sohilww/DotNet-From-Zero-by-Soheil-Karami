b55825d066b8fa656604ea93cb5371d748952d11
Soheil Karami <soheilkarami@outlook.com>
2025-07-12 01:18:43 +0330

    session 1 file has been added to project

--- docs/DotNetFromZeroBySoheilKarami/docusaurus.config.js
+++ docs/DotNetFromZeroBySoheilKarami/docusaurus.config.js
@@ -27,9 +27,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
-
+  organizationName: "sohilww", // Usually your GitHub org/user name.
+  projectName: "DotNet-From-Zero-by-Soheil-Karami", // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
