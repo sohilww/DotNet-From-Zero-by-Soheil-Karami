cd258a922644e1dc05364736ec13028c2b2118b5
Nastaran Naderi <Naderi.Nastaran@gmail.com>
2025-09-17 23:23:03 +0330

    Create models for "manage Appointment"

---
+++ codes/ClinicAppointmentSystem/src/CAS.Domain/Models/Appointment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.Domain.Models;
+
+public class Appointment
+{
+    public Guid Id { get; private set; }
+    public Guid DoctorId { get; private set; }
+    public Guid PatientId { get; private set; }
+    public DateTime Date { get; private set; }
+    public Guid PeriodId { get; private set; }
+    public AppointmentStatus Status { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime ModifiedAt { get; private set; }
+
+    public Appointment(Guid id, Guid doctorId, Guid patientId, DateTime date, Guid periodId)
+    {
+        Id = id;
+        DoctorId = doctorId;
+        PatientId = patientId;
+        Date = date;
+        PeriodId = periodId;
+        Status = AppointmentStatus.Reserved;
+        CreatedAt = DateTime.UtcNow;
+        ModifiedAt = DateTime.UtcNow;
+    }
+
+    public void Confirm()
+    {
+        Status = AppointmentStatus.Confirmed;
+        ModifiedAt = DateTime.UtcNow;
+    }
+
+    public void Cancel()
+    {
+        Status = AppointmentStatus.Canceled;
+        ModifiedAt = DateTime.UtcNow;
+    }
+}
