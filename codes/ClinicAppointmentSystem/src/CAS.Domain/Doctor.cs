3cf532aa1199d81e8a84b26884cecfdd1b67b6bb
faezeh ebrahimy <fezebr2000@gmail.com>
2025-09-18 22:04:50 +0330

    refactor: rename codeMeli to NationalCode

--- codes/ClinicAppointmentSystem/src/CAS.Domain/Doctor.cs
+++ codes/ClinicAppointmentSystem/src/CAS.Domain/Doctor.cs
@@ -6,16 +6,16 @@ public class Doctor
     public string Name { get; }
     public string Lastname { get; }
     public string Expertise { get; }
-    public string CodeMeli { get; set; }
+    public string NationalCode { get; set; }
     public IReadOnlyList<int> WorkingDays { get; }
 
-    public Doctor(string name, string lastname, string expertise, string codeMeli, List<int> workingDays)
+    public Doctor(string name, string lastname, string expertise, string nationalCode, List<int> workingDays)
     {
         Id = Guid.NewGuid();
         Name = name ?? throw new ArgumentNullException(nameof(name));
         Lastname = lastname ?? throw new ArgumentNullException(nameof(lastname));
         Expertise = expertise ?? throw new ArgumentNullException(nameof(expertise));
-        CodeMeli = codeMeli ?? throw new ArgumentNullException(nameof(CodeMeli));   
+        NationalCode = nationalCode ?? throw new ArgumentNullException(nameof(NationalCode));   
         WorkingDays = workingDays?.AsReadOnly() ?? throw new ArgumentNullException(nameof(workingDays));
     }
 
